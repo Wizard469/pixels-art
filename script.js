df9f4871869678992c66a3d5003498f76e00ccdd
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-04-20 17:04:00 -0300

    Second n third requirement

---
+++ script.js
@@ -0,0 +1,9 @@
+let color1 = document.getElementsByClassName('color')[0];
+let color2 = document.getElementsByClassName('color')[1];
+let color3 = document.getElementsByClassName('color')[2];
+let color4 = document.getElementsByClassName('color')[3];
+
+color1.style.backgroundColor = 'black';
+color2.style.backgroundColor = 'red';
+color3.style.backgroundColor = 'blue';
+color4.style.backgroundColor = 'green';
